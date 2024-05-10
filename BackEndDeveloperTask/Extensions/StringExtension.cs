9a92d6df1c500062ccfd03e3ad0be691216027e7
Dan Booth <dan@diplo.co.uk>
2024-05-10 12:36:53 +0100

    Better to make this an extension method so it's neater and easier to modify / reuse. Also, need to check length before using substring otherwise you'll get an exception if less than 100 chars. Return IHtmlContent so it's definitely ecncoded and special chars converted.

--- BackEndDeveloperTask/Extensions/StringExtension.cs
+++ BackEndDeveloperTask/Extensions/StringExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net;
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Html;
 
 namespace BackEndDeveloperTask.Extensions
 {
@@ -9,6 +10,25 @@ namespace BackEndDeveloperTask.Extensions
         {
             var result = WebUtility.HtmlDecode(input);
             return Regex.Replace(result, "<.*?>", string.Empty);
+        }
+
+        public static IHtmlContent Truncate(this string input, int length = 100)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return new HtmlString(string.Empty);
+            }
+
+            input = input.StripHTML();
+
+            if (input.Length > length)
+            {
+                return new HtmlString(input[..100] + "...");
+            }
+            else
+            {
+                return new HtmlString(input);
+            }
         }
     }
 }
